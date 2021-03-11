220962f6b1d77d00140530e4dd943511216c9eb2
ripon <riponahmed2201@gmail.com>
2021-03-11 18:57:05 +0600

    typing issue fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>TyPingprogram</title>
+    <title>TypingProgram</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -27,7 +27,7 @@
             <div class="container">
                 
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <strong style="font-size: 25px; font-family: 'Roboto', sans-serif;">TyPingprogram</strong>
+                    <strong style="font-size: 25px; font-family: 'Roboto', sans-serif;">TypingProgram</strong>
                 </a>
                 @auth
                 <a class="navbar-brand" href="{{ route('document.index') }}">
